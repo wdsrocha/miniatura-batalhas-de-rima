2125020bdc9c29b55924b13d47e62d55d5abfad0
Wesley Rocha <wesleysr1997@gmail.com>
2023-04-28 04:07:29 -0400

    Refactor Upload component in its own file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { ArrowUpTrayIcon, TrashIcon } from "@heroicons/react/24/solid";
+import { Upload } from "@/components/Upload";
+import { TrashIcon } from "@heroicons/react/24/solid";
 import cn from "classnames";
 import { toPng } from "html-to-image";
 import localFont from "next/font/local";
@@ -242,34 +243,9 @@ export default function Home() {
         }}
       >
         <div className="space-y-2">
-          <label htmlFor="image">
-            <span
-              role="button"
-              aria-controls="filename"
-              tabIndex={0}
-              onKeyDown={handleKeyDown}
-              className="inline-flex items-center gap-x-1.5 rounded-md bg-white/10 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-white/20"
-            >
-              <ArrowUpTrayIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
-              Escolher imagem
-            </span>
-          </label>
-          <input
-            tabIndex={-1}
-            id="image"
-            name="image"
-            type="file"
-            className="sr-only"
-            onClick={(event) => {
-              // This is a hack to allow the user to select the same file twice
-              // https://stackoverflow.com/a/58527761
-              // @ts-ignore
-              event.target.value = null;
-            }}
-            onChange={handleImageChange}
-          />
+          <Upload handleFiles={handleFiles} />
 
-          {image && (
+          {imageFilename && (
             <div className="flex justify-between items-center">
               <p className="text-sm leading-6 text-gray-400">{imageFilename}</p>
               <button
