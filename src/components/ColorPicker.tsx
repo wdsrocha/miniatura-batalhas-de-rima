6d933d3689ce114939639fc2edf97aa744f88b93
Wesley Rocha <wesleysr1997@gmail.com>
2023-07-21 15:36:51 -0400

    Use drawer for mobile color pick
    It looks quite bad because the drawer size is hardcoded (the top of the drawer stays at 40% of screen height). This leads to a chunk of nothing appearing on screen or even make some content go missing in smaller screens/bigger texts

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,4 +1,5 @@
 import { Input } from "./Input";
+import { SlideOver } from "./SlideOver";
 import { Popover, RadioGroup, Transition } from "@headlessui/react";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import cn from "classnames";
@@ -39,8 +40,52 @@ const isLight = (hexColor: string | undefined) => {
   return luma > 128;
 };
 
+const CustomColorButton = (props: {
+  customColor?: string;
+  onClick?: () => void;
+}) => {
+  const { customColor, onClick } = props;
+  return (
+    <RadioGroup.Option
+      onClick={onClick}
+      value={customColor}
+      className={({ active, checked }) =>
+        cn(
+          "ring-gray-400",
+          checked && active ? "ring" : "",
+          checked && !active ? "ring-2" : "",
+          "relative flex items-center justify-center",
+          "cursor-pointer rounded-md p-0.5 hover:brightness-150 focus:outline-none"
+        )
+      }
+    >
+      <RadioGroup.Label as="span" className="sr-only">
+        Cor customizada referente ao código hex {customColor}
+      </RadioGroup.Label>
+      <span
+        aria-hidden="true"
+        style={{
+          backgroundColor: customColor,
+        }}
+        className={cn(
+          "flex h-8 w-8 items-center justify-center rounded",
+          !customColor && "border border-white"
+        )}
+      >
+        <PlusIcon
+          className={cn(
+            "h-6 w-6",
+            isLight(customColor) ? "text-black" : "text-white"
+          )}
+        />
+      </span>
+    </RadioGroup.Option>
+  );
+};
+
 export const ColorPicker = (props: Props) => {
   const [customColor, setCustomColor] = useState<string | undefined>();
+  const [pickerOpen, setPickerOpen] = useState(false);
 
   const onCustomColorChange = (color: string) => {
     setCustomColor(color);
@@ -83,69 +128,63 @@ export const ColorPicker = (props: Props) => {
             />
           </RadioGroup.Option>
         ))}
-        <Popover>
-          <Popover.Button>
-            <RadioGroup.Option
-              value={customColor}
-              className={({ active, checked }) =>
-                cn(
-                  "ring-gray-400",
-                  checked && active ? "ring" : "",
-                  checked && !active ? "ring-2" : "",
-                  "relative flex items-center justify-center",
-                  "cursor-pointer rounded-md p-0.5 hover:brightness-150 focus:outline-none"
-                )
-              }
-            >
-              <RadioGroup.Label as="span" className="sr-only">
-                Cor customizada referente ao código hex {customColor}
-              </RadioGroup.Label>
-              <span
-                aria-hidden="true"
+
+        <div className="block md:hidden ">
+          <CustomColorButton
+            customColor={customColor}
+            onClick={() => setPickerOpen(true)}
+          />
+          <SlideOver open={pickerOpen} setOpen={setPickerOpen}>
+            <div className="space-y-4">
+              <HexColorPicker
                 style={{
-                  backgroundColor: customColor,
+                  width: "auto",
                 }}
-                className={cn(
-                  "flex h-8 w-8 items-center justify-center rounded",
-                  !customColor && "border border-white"
-                )}
-              >
-                <PlusIcon
-                  className={cn(
-                    "h-6 w-6",
-                    isLight(customColor) ? "text-black" : "text-white"
-                  )}
-                />
-              </span>
-            </RadioGroup.Option>
-          </Popover.Button>
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-200"
-            enterFrom="opacity-0 translate-y-1"
-            enterTo="opacity-100 translate-y-0"
-            leave="transition ease-in duration-150"
-            leaveFrom="opacity-100 translate-y-0"
-            leaveTo="opacity-0 translate-y-1"
-          >
-            <Popover.Panel className="absolute z-10">
-              <div className="flex flex-col items-center gap-y-4 rounded-lg bg-gray-700 pb-3">
-                <HexColorPicker
-                  color={customColor}
-                  onChange={onCustomColorChange}
-                />
-                <div className="px-2">
-                  <Input
-                    id="customColorPicker"
-                    label="Hex"
-                    value={customColor || ""}
-                    onChange={(e) => onCustomColorChange(e.target.value)}
+                color={customColor}
+                onChange={onCustomColorChange}
+              />
+              <Input
+                id="customColorPicker"
+                label="Hex"
+                value={customColor || ""}
+                onChange={(e) => onCustomColorChange(e.target.value)}
+              />
+            </div>
+          </SlideOver>
+        </div>
+        <div className="hidden md:block">
+          <Popover>
+            <Popover.Button>
+              <CustomColorButton customColor={customColor} />
+            </Popover.Button>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute z-10">
+                <div className="flex flex-col items-center gap-y-4 rounded-lg bg-gray-700 pb-3">
+                  <HexColorPicker
+                    color={customColor}
+                    onChange={onCustomColorChange}
                   />
+                  <div className="px-2">
+                    <Input
+                      id="customColorPicker"
+                      label="Hex"
+                      value={customColor || ""}
+                      onChange={(e) => onCustomColorChange(e.target.value)}
+                    />
+                  </div>
                 </div>
-              </div>
-            </Popover.Panel>
-          </Transition>
-        </Popover>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+        </div>
       </div>
     </RadioGroup>
   );
---
+++ src/components/SlideOver.tsx
@@ -0,0 +1,60 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import { Fragment, ReactNode, useState } from "react";
+
+interface Props {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  children?: ReactNode;
+}
+
+export function SlideOver(props: Props) {
+  return (
+    <Transition.Root show={props.open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={props.setOpen}>
+        <div className="fixed inset-0" />
+
+        <div className="fixed inset-0 overflow-hidden">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pt-8">
+              <Transition.Child
+                as={Fragment}
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enterFrom="translate-y-full"
+                enterTo="translate-y-[40%]"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leaveFrom="translate-y-0"
+                leaveTo="translate-y-full"
+              >
+                <Dialog.Panel className="pointer-events-auto w-screen">
+                  <div className="flex h-full flex-col overflow-y-scroll bg-gray-700 py-6 shadow-xl">
+                    <div className="px-4 sm:px-6">
+                      <div className="flex items-start justify-between">
+                        <Dialog.Title className="text-base font-semibold leading-6 text-white">
+                          Cor de destaque
+                        </Dialog.Title>
+                        <div className="ml-3 flex h-7 items-center">
+                          <button
+                            type="button"
+                            className="rounded-md bg-gray-700 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            onClick={() => props.setOpen(false)}
+                          >
+                            <span className="sr-only">Close panel</span>
+                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                      {props.children}
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
